d5cdc1a2c94c174e142f6d0cc4bc78db4d0f4f43
Chinmay <chinmay.patil3@walchandsangli.ac.in>
2025-01-23 01:16:19 +0530

    added /delete-data route

--- src/components/AddVideoDetail.jsx
+++ src/components/AddVideoDetail.jsx
@@ -32,41 +32,41 @@ function AddVideoDetail() {
     return (
         <div className="p-4 text-xl">
             <form onSubmit={handleSubmit}>
-                <label htmlFor="vlabel">Add video title</label><br/>
-                <input 
+                <label htmlFor="vlabel">Add video title</label><br />
+                <input
                     id='vlabel'
                     name='vlabel'
-                    type="text" 
+                    type="text"
                     value={formData.vlabel}
-                    className="border-pink-700 border-solid border-2 block my-3 w-full" 
+                    className="border-pink-700 border-solid border-2 block my-3 w-full"
                     onChange={handleChange}
-                    required/>
+                    required />
 
-                <label htmlFor="vlink">Paste video link</label><br/>
-                <input 
+                <label htmlFor="vlink">Paste video link</label><br />
+                <input
                     id='vlink'
                     name='vlink'
-                    type="text" 
+                    type="text"
                     value={formData.vlink}
-                    className="border-pink-700 border-solid border-2 block my-3 w-full" 
+                    className="border-pink-700 border-solid border-2 block my-3 w-full"
                     onChange={handleChange}
-                    required/>
+                    required />
 
-                <label htmlFor="postedby">Your codename</label><br/>
-                <input 
+                <label htmlFor="postedby">Your codename</label><br />
+                <input
                     id='postedby'
                     name='postedby'
-                    type="text" 
+                    type="text"
                     value={formData.postedby}
-                    className="border-pink-700 border-solid border-2 block my-3 w-full" 
+                    className="border-pink-700 border-solid border-2 block my-3 w-full"
                     onChange={handleChange}
-                    required/>
+                    required />
 
                 <button type="submit" className="bg-pink-500 text-white px-4 py-2 mt-8 block text-center w-full">Submit</button>
             </form>
 
-            <button className="mt-3 block text-red-500 underline" onClick={() => {navigate('/home')}}>Home</button>
-            
+            <button className="mt-3 block text-red-500 underline" onClick={() => { navigate('/home') }}>Home</button>
+
         </div>
     );
 }
